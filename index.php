0815f2b24f289b23db429e56d0f8b8d99633f01c
JuIiet <Juliet_065@hotmail.com>
2017-07-19 13:39:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'Send all device.'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
