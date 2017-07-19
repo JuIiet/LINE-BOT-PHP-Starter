10c9be1e43e7383e398a406a2e46898b8214faf1
JuIiet <Juliet_065@hotmail.com>
2017-07-19 15:04:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,9 +56,9 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'replyToken' => $replyToken,
+				'to' => '1165ddffef854c75b22aa7315b1a559c',
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
