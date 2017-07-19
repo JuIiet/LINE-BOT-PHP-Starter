a6d5463d4fb05fe0aebd2a596b9bb37c971d9c04
JuIiet <Juliet_065@hotmail.com>
2017-07-19 14:43:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -48,8 +48,9 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => 'https://github.com/apple-touch-icon.png'
+				'type' => 'image',
+				'originalContentUrl' => 'https://github.com/apple-touch-icon.png'
+				'previewImageUrl' => 'https://github.com/apple-touch-icon.png'
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
