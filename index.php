4065b16dcbaece62240919a44e29fe777e44d329
JuIiet <Juliet_065@hotmail.com>
2017-07-19 14:50:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'https://github.com/apple-touch-icon.png'
+				'originalContentUrl' => 'https://github.com/apple-touch-icon.png',
+				'previewImageUrl' => 'https://github.com/apple-touch-icon.png'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
