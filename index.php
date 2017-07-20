cca598375686a2e7f659c4c107e3cea75f98016d
JuIiet <Juliet_065@hotmail.com>
2017-07-20 10:59:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			//	'text' => $userId
 			//];
 			
-			$messages = array('type' => 'text','text' => $userId);
+			$messages = array("type" => "text","text" => $userId);
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -34,7 +34,7 @@ if (!is_null($events['events'])) {
 			//	'replyToken' => $replyToken,
 			//	'messages' => [$messages]
 			//];
-			$data = array('replyToken' => $replyToken,'messages' => {$messages});
+			$data = array('replyToken' => $replyToken,'messages' => [$messages]);
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -132,5 +132,5 @@ if (!is_null($events['events'])) {
 
 	}
 }
-echo "OK3";
+echo "OK1";
 ?>
